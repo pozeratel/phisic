04a8e1a354300012c4f453ded9af9324d4b5795d
pozeratel <svobodavaha@gmail.com>
2024-10-27 23:40:15 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/sass/main.css">
+</head>
+
+<body>
+    <?php
+    require_once("sample/header.php")
+        ?>
+
+
+
+
+
+
+
+</body>
+
+</html>
---
+++ sample/header.php
@@ -0,0 +1,27 @@
+<header>
+    <div class="container">
+        <div class="header-top">
+            <nav>
+                <div class="nav-items">
+                    <a href="#" class="nav-links">Головна</a>
+                </div>
+                <div class="nav-items">
+                    <a href="#" class="nav-links">На уроці</a>
+                </div>
+                <div class="nav-items">
+                    <a href="#" class="nav-links">Фізичні івенти</a>
+                </div>
+                <div class="nav-items">
+                    <a href="#" class="nav-links">Документи</a>
+                </div>
+                <div class="nav-items">
+                    <a href="#" class="nav-links">Цікаві факти</a>
+                </div>
+                <div class="nav-items">
+                    <a href="#" class="nav-links">Наукова робота</a>
+                </div>
+            </nav>
+        </div>
+    </div>
+   
+</header>
